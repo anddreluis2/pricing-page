12cd4038cbd492819768e1bd6294b9c7d36bae7e
andre <anddreluis98@gmail.com>
2022-08-11 01:03:30 -0300

    setting the toggle for switch button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,14 @@
+import React, { useState } from 'react'
 import styles from './styles.module.scss'
 import Switch from "react-switch"
 
 export function Header() {
+    const [subscription, setSubscription] = useState(false)
+
+    const toggleSubscriptionType = () => {
+        setSubscription(subscription == true ? false : true)
+        console.log("You changed the subscription!")
+    }
     return (
         <>
             <div className={styles.container}>
@@ -9,12 +16,17 @@ export function Header() {
                 <p>Try it now for free</p>
             </div>
             <div className={styles.wrapper}>
+                <span>Monthly</span>
                 <Switch
-                    onChange={() => { console.log("Aaaaa") }}
-                    checked={true}
+                    onChange={() => { toggleSubscriptionType() }}
+                    checked={subscription}
                     checkedIcon={false}
                     uncheckedIcon={false}
+                    offColor="#5e5e5e"
+                    onColor="#2f2f2f"
                 />
+                <span>Annually</span>
+                <p className={styles.subtitle}>(15% off)</p>
             </div>
         </>
 
